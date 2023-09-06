f39d66df78107f774a6d737d22530cb214e0f35f
AL-NFLY <almernfly@gmail.com>
2023-09-06 18:45:34 +0700

    Popular Product Cards added

---
+++ src/components/PopularProductCard.jsx
@@ -0,0 +1,16 @@
+import { star } from "../assets/icons"
+const PopularProductCard = ({imgURL, name, price}) => {
+  return (
+    <div className="flex flex-1 flex-col w-full max-sm:w-full">
+        <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
+        <div className="flex justify-start gap-2.5 mt-8">
+            <img src={star} alt="rating" width={24} height={24} />
+            <p className="font-montserrat text-xl leading-normal text-slate-gray">(4.5)</p>
+        </div>
+        <h3 className="mt-2 text-2xl leading-normal font-palanquin font-semibold ">{name}</h3>
+        <p className="text-xl leading-normal font-montserrat font-semibold text-coral-red">{price}</p>
+    </div>
+  )
+}
+
+export default PopularProductCard
