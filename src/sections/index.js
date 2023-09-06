5747dd203eb931b3f0b26823f64a29987d4e6b24
AL-NFLY <almernfly@gmail.com>
2023-09-06 23:00:50 +0700

    CustomerReviews section deleted completely

--- src/sections/index.js
+++ src/sections/index.js
@@ -3,7 +3,6 @@ import PopularProducts from './PopularProducts'
 import SuperQuality from './SuperQuality'
 import Services from './Services'
 import SpecialOffers from './SpecialOffers'
-import CustomerReviews from './CustomerReviews'
 import Subscribe from './Subscribe'
 import Footer from './Footer'
 
@@ -13,7 +12,6 @@ export{
     SuperQuality,
     Services,
     SpecialOffers,
-    CustomerReviews,
     Subscribe,
     Footer,
 }
