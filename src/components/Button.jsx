ee935d63c5481282d3e70b197889f779a7653b82
AL-NFLY <almernfly@gmail.com>
2023-09-06 23:46:44 +0700

    Subscribe section completed

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,4 @@
-const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor, fullWidth }) => {
   return (
     <button 
       className={`flex justify-center items-center gap-2 px-6 py-3 border text-lg rounded-md leading-none font-montserrat 
@@ -6,9 +6,11 @@ const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }) => 
           backgroundColor
             ? `${backgroundColor} ${borderColor} ${textColor} `
             : 'bg-coral-red border-coral-red text-white'
-        }
+        } ${fullWidth && 'w-full'}
       `}>
+
       {label}
+
       {iconURL && <img 
       src={iconURL} 
       alt="arrow right icon"
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,6 +1,29 @@
+import Button from "../components/Button"
+
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <section id="contact-us" className="max-container flex justify-between items-center flex-col gap-10">
+      <h3 className="text-4xl font-palanquin font-bold leading-[68px]">
+        Sign up for 
+        <span className="text-coral-red"> Updates </span>
+        & Newsletter
+      </h3>
+
+      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded">
+        <input 
+          type="text"
+          placeholder="almer.nfly@gmail.com"
+          className="input"
+        />
+
+        <div className="flex items-center max-sm:justify-end max-sm:w-full">
+        <Button 
+          label="Sign Up"
+          fullWidth
+        />
+        </div>
+      </div>
+    </section>
   )
 }
 
