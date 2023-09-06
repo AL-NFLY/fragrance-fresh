4e2b6265f5c2a562798bed71fb3c206732636736
AL-NFLY <almernfly@gmail.com>
2023-09-06 22:57:13 +0700

    CustomerReviews section deleted & added bg color to Services section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Hero, PopularProducts, SuperQuality, Services, SpecialOffers, CustomerReviews, Subscribe, Footer } from "./sections";
+import { Hero, PopularProducts, SuperQuality, Services, SpecialOffers, Subscribe, Footer } from "./sections";
 import Nav from "./components/Nav";
 
 const App = () =>(
@@ -8,20 +8,17 @@ const App = () =>(
     <section className="xl:padding-l wide:padding-r padding-b">
       <Hero />
     </section>
-    <section className="padding">
+    <section className="padding ">
       <PopularProducts />
     </section>
     <section className="padding">
       <SuperQuality />
     </section>
-    <section className="padding-x py-10">
+    <section className="padding-x py-10 bg-pale-blue">
       <Services />
     </section>
     <section className="padding">
       <SpecialOffers />
-    </section>
-    <section className="bg-pale-blue padding">
-      <CustomerReviews />
     </section>
     <section className="padding-x sm:py-32 py-16 w-full">
       <Subscribe />
