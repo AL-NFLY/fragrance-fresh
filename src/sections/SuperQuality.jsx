1eb4962f0132edd6c9c972ea00a9ef66f3f5bde7
AL-NFLY <almernfly@gmail.com>
2023-09-09 23:25:08 +0700

    SuperQuality section adjusted for dark mode

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -7,7 +7,7 @@ const SuperQuality = () => {
           <h2 className="mt-8 font-bauerbodoni font-bold text-4xl capitalize lg:max-w-lg">
             Our Philosophy
           </h2>
-          <div className="font-postgrotesk text-slate-gray leading-8 text-lg lg:max-w-2xl">
+          <div className="font-postgrotesk text-slate-gray dark:text-white-400 leading-8 text-lg lg:max-w-2xl">
             <p className="mt-8 max-sm:mt-6">Composed like a fragrance wardrobe, each Maison Francis Kurkdjian collection is like the olfactory signature of an emotion. The Maison is guided by enchanting, yet precise codes: purity, sophistication, timelessness and the boldness of a neo-classicism .</p>
             <p className="mt-6 max-sm:mt-4">Exceptional know-how come together and echo off each other. They are continually writing a number of sensory adventures, in which perfume is, naturally, the hero.</p>
             <p className="mt-6 max-sm:mt-4">Our dedication towards reaching that olfactory scent is paramount</p>
